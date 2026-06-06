f2786f8cb39a160a46a25c390acf15719d068f12
GboyYouMam <kozahar185@gmail.com>
2026-06-06 03:43:01 +0300

    feat: minio storage for pfps

---
+++ src/storage/storage.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as Minio from 'minio';
+
+@Injectable()
+export class StorageService {
+    private minioClient: Minio.Client;
+    private bucketName: string;
+    private readonly logger = new Logger(StorageService.name);
+
+    constructor(private configService: ConfigService) {
+        this.minioClient = new Minio.Client({
+            endPoint: this.configService.get<string>('MINIO_ENDPOINT') || '127.0.0.1',
+            port: Number(this.configService.get<number>('MINIO_PORT')) || 9000,
+            useSSL: false,
+            accessKey: this.configService.get<string>('MINIO_ACCESS_KEY') as string,
+            secretKey: this.configService.get<string>('MINIO_SECRET_KEY') as string,
+        });
+        this.bucketName = this.configService.get<string>('MINIO_BUCKET_NAME') || 'avatars';
+    }
+
+    async onModuleInit() {
+        const exists = await this.minioClient.bucketExists(this.bucketName);
+        if (!exists) {
+            await this.minioClient.makeBucket(this.bucketName, 'us-east-1');
+
+            const policy = {
+                Version: '2012-10-17',
+                Statement: [
+                    {
+                        Effect: 'Allow',
+                        Principal: { AWS: ['*'] },
+                        Action: ['s3:GetObject'],
+                        Resource: [`arn:aws:s3:::${this.bucketName}/*`],
+                    },
+                ],
+            };
+            await this.minioClient.setBucketPolicy(this.bucketName, JSON.stringify(policy));
+            this.logger.log(`public bucket created: ${this.bucketName}`);
+        }
+    }
+
+    async uploadAvatar(file: Express.Multer.File, userId: string): Promise<string> {
+        try {
+            const extension = file.originalname.split('.').pop();
+            const fileName = `${userId}-${Date.now()}.${extension}`;
+
+            await this.minioClient.putObject(
+                this.bucketName,
+                fileName,
+                file.buffer,
+                file.size,
+                { 'Content-Type': file.mimetype }
+            );
+
+            const endPoint = this.configService.get('MINIO_ENDPOINT');
+            const port = this.configService.get('MINIO_PORT');
+
+            return `http://${endPoint}:${port}/${this.bucketName}/${fileName}`;
+        } catch (error) {
+            this.logger.error('error while uploading to MinIO', error);
+            throw new InternalServerErrorException('Cannot save this file lmao');
+        }
+    }
+}
