a90aefe10e68b27069ecee6cbd8226ca755f7698
GboyYouMam <kozahar185@gmail.com>
2026-06-08 02:58:38 +0300

    hotfix: changed some nuances

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import {UpdateUserData, UsersRepository} from '../auth/users.repository';
+import { StorageService } from '../storage/storage.service';
+import {UpdateProfileDto} from "./dto/update-profile.dto";
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly usersRepository: UsersRepository) {}
+    constructor(
+        private readonly usersRepository: UsersRepository,
+        private readonly storageService: StorageService
+    ) {}
 
     async getUserProfile(userId: string) {
         const user = await this.usersRepository.findById(userId);
@@ -15,12 +20,25 @@ export class UsersService {
         return user;
     }
 
-    async updateProfile(userId: string, data: UpdateUserData) {
+    async updateProfile(userId: string, data: UpdateProfileDto, file?: Express.Multer.File) {
         const user = await this.usersRepository.findById(userId);
         if (!user) {
-            throw new NotFoundException('Користувача не знайдено');
+            throw new NotFoundException('User not found');
         }
 
-        return this.usersRepository.updateProfile(userId, data);
+        const updatePayload: UpdateUserData = { ...data };
+
+        if (file) {
+            const fileData = await this.storageService.uploadFile(file, userId);
+            updatePayload.pfp_url = fileData.url;
+        }
+
+        const updatedUser = await this.usersRepository.updateProfile(userId, updatePayload);
+
+        if ('password' in updatedUser) {
+            delete (updatedUser as any).password;
+        }
+
+        return updatedUser;
     }
 }
