59556ca74a0ef8e4cc0e8dd80e44ed5e63d5713e
GboyYouMam <kozahar185@gmail.com>
2026-06-09 11:51:44 +0300

    small changes in import part

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StorageModule } from './storage/storage.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
