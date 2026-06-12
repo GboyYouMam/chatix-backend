d4f0a97c2fa65480c845a95503a6f35cf312a987
GboyYouMam <kozahar185@gmail.com>
2026-06-12 21:02:48 +0300

    idk

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StorageModule } from './storage/storage.module';
 import { AdminModule } from './admin/admin.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [AdminModule, StorageModule, ConfigModule.forRoot({isGlobal: true})],
