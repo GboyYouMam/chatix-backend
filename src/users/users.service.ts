fec6f517cb52230862940f9890bbaa706a640fb0
GboyYouMam <kozahar185@gmail.com>
2026-07-27 05:04:19 +0300

    patch: found that canChangeProfile wasnt even checking while updating profile data, so fix that

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,72 +1,89 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import {UpdateUserData, UsersRepository} from '../auth/users.repository';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { UpdateUserData, UsersRepository } from '../auth/users.repository';
 import { StorageService } from '../storage/storage.service';
-import {UpdateProfileDto} from "./dto/update-profile.dto";
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        private readonly usersRepository: UsersRepository,
-        private readonly storageService: StorageService
-    ) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly storageService: StorageService,
+  ) {}
 
-    async getUserProfile(userId: string) {
-        const user = await this.usersRepository.findById(userId);
+  async getUserProfile(userId: string) {
+    const user = await this.usersRepository.findById(userId);
 
-        if (!user) {
-            throw new NotFoundException('Foid not found');
-        }
-
-        return user;
+    if (!user) {
+      throw new NotFoundException('Foid not found');
     }
 
-    async updateProfile(userId: string, data: UpdateProfileDto, file?: Express.Multer.File) {
-        const user = await this.usersRepository.findById(userId);
-        if (!user) {
-            throw new NotFoundException('User not found');
-        }
+    return user;
+  }
 
-        const updatePayload: UpdateUserData = { ...data };
-
-        if (file) {
-            const fileData = await this.storageService.uploadFile(file, userId);
-            updatePayload.pfp_url = fileData.url;
-        }
-
-        const updatedUser = await this.usersRepository.updateProfile(userId, updatePayload);
-
-        if ('password' in updatedUser) {
-            delete (updatedUser as any).password;
-        }
-
-        return updatedUser;
+  async updateProfile(
+    userId: string,
+    data: UpdateProfileDto,
+    file?: Express.Multer.File,
+  ) {
+    const user = await this.usersRepository.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
 
-    async findByUsername(username: string) {
-        const user = await this.usersRepository.findByUsername(username);
-
-        if (!user) {
-            throw new NotFoundException('This user not found ughhhh');
-        }
-        const { password, ...safeUser } = user;
-        return safeUser;
+    if (!user.canChangeProfile) {
+      throw new ForbiddenException(
+        'Admins says that u DONT HAVE ANY RIGHTS TO DO IT, go cry about it',
+      );
     }
 
-    async auraFarming(userId: string) {
-        const user = await this.usersRepository.findById(userId);
-        if (!user) {
-            throw new NotFoundException('User not found');
-        }
+    const updatePayload: UpdateUserData = { ...data };
 
-        const newAura = Number(user.aura) + 1;
-
-        const updatedUser = await this.usersRepository.updateProfile(userId, {
-            aura: newAura,
-        } as any);
-
-        return {
-            aura: newAura,
-            message: "aura farmed"
-        };
+    if (file) {
+      const fileData = await this.storageService.uploadFile(file, userId);
+      updatePayload.pfp_url = fileData.url;
     }
+
+    const updatedUser = await this.usersRepository.updateProfile(
+      userId,
+      updatePayload,
+    );
+
+    if ('password' in updatedUser) {
+      delete (updatedUser as any).password;
+    }
+
+    return updatedUser;
+  }
+
+  async findByUsername(username: string) {
+    const user = await this.usersRepository.findByUsername(username);
+
+    if (!user) {
+      throw new NotFoundException('This user not found ughhhh');
+    }
+    const { password, ...safeUser } = user;
+    return safeUser;
+  }
+
+  async auraFarming(userId: string) {
+    const user = await this.usersRepository.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const newAura = Number(user.aura) + 1;
+
+    const updatedUser = await this.usersRepository.updateProfile(userId, {
+      aura: newAura,
+    } as any);
+
+    return {
+      aura: newAura,
+      message: 'aura farmed',
+    };
+  }
 }
